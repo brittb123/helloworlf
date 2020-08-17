2ccdc9cdee0149c039784767a0e5ab73afba7bd8
Britt Boudreaux <brittboudreaux@yahoo.com>
2020-08-17 16:27:17 -0500

    added player stats and name input

--- Game.cs
+++ Game.cs
@@ -8,7 +8,23 @@ namespace HelloWorld
     {
         public void Run()
         {
-
+            float health = 100.0f;
+            float healthRegen = 20;
+            Console.Write("What is your name traveler?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Is this your name?");
+            Console.ReadLine();
+            bool maxLevelReached = false;
+            int maxlevel = 100;
+            maxlevel = 120;
+            int level = maxlevel;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+         
+            Console.Write(name + " has healed " + healthRegen);
+            
+        
         }
     }
 }
